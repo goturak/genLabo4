8718fd1becd74d75aba2e9b54ea25807470da700
luca de Carvalho <luca.de.carvalho@gmail.com>
2019-05-21 19:33:00 +0200

    moved and renamed getSizeFor to product

--- src/main/java/ch/heigvd/gen2019/Product.java
+++ src/main/java/ch/heigvd/gen2019/Product.java
@@ -36,5 +36,22 @@ public class Product {
         return currency;
     }
 
-
+    public String getSizeString() {
+        switch (getSize()) {
+            case 1:
+                return "XS";
+            case 2:
+                return "S";
+            case 3:
+                return "M";
+            case 4:
+                return "L";
+            case 5:
+                return "XL";
+            case 6:
+                return "XXL";
+            default:
+                return "Invalid Size";
+        }
+    }
 }
