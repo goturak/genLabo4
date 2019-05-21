afb10f9d03f3215f9d563395339b7194db1e9011
luca de Carvalho <luca.de.carvalho@gmail.com>
2019-05-21 20:03:55 +0200

    added correct toString in order and removed duplicate code from orderswriter

--- src/main/java/ch/heigvd/gen2019/Order.java
+++ src/main/java/ch/heigvd/gen2019/Order.java
@@ -26,4 +26,26 @@ public class Order {
     public void AddProduct(Product product) {
         products.add(product);
     }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("{");
+        sb.append("\"id\": ");
+        sb.append(getOrderId());
+        sb.append(", ");
+        sb.append("\"products\": [");
+        for (int j = 0; j < getProductsCount(); j++) {
+            Product product = getProduct(j);
+            sb.append(product);
+            sb.append(", ");
+        }
+
+        if (getProductsCount() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        sb.append("]");
+        return sb.toString();
+    }
 }
