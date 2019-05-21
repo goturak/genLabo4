04c72e51f9c154968f5c66a347f868139a667e9f
luca de Carvalho <luca.de.carvalho@gmail.com>
2019-05-21 20:08:21 +0200

    added correct toString in orders and removed duplicate code from ordersWriter

--- src/main/java/ch/heigvd/gen2019/Orders.java
+++ src/main/java/ch/heigvd/gen2019/Orders.java
@@ -17,4 +17,21 @@ public class Orders {
     public Order getOrder(int i) {
         return orders.get(i);
     }
+
+    @Override
+    public String toString() {
+        StringBuffer sb = new StringBuffer("{\"orders\": [");
+
+        for (int i = 0; i < getOrdersCount(); i++) {
+            Order order = getOrder(i);
+            sb.append(order);
+            sb.append("}, ");
+        }
+
+        if (getOrdersCount() > 0) {
+            sb.delete(sb.length() - 2, sb.length());
+        }
+
+        return sb.append("]}").toString();
+    }
 }
